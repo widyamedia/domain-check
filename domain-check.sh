a9a1b382d831d26b9b106e1da58de6da1c0c8de2
widyamedia <widyamedia@gmail.com>
2019-09-01 11:12:27 +0700

    add domain-check.sh and domains file

---
+++ domain-check.sh
@@ -0,0 +1,2 @@
+cd /data/progs/domain-check
+/data/progs/pythonve/domain-check/bin/python dns-domain-expiration-checker.py --domainfile domains --email --smtpto "admin@indonesiadot.com" --smtpfrom "root@mail-124.indonesiadot.com"
